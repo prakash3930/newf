2dfbb67e534468f82077adbb740fbec2e6a48c79
alex <lionwild@gmail.com>
2023-05-11 12:51:15 +0600

    change

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const body = require('body-parser');
 const app = express();
 const PORT = 3000;
-app.use(express.json());
+app.use(body.json());
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -16,23 +17,29 @@ app.get("/books", (req, res) => {
 app.post("/books", (req, res) => {
   const { title, author, publishedDate } = req.body;
 
-  const id = Date.now().toString();
+  const random = Math.floor(Math.random()*10000000000000)+1;
+  const id = random.toString(36);
 
-  books.push({ id, title, author, publishedDate });
+  if(!title || !author){
+    res.json(`Title and Author is require....`);
+  }
+  else{
+    books.push({ id, title, author, publishedDate });
 
-  res.json({ id, title, author, publishedDate });
+    res.json({ id, title, author, publishedDate });
+  }
 });
 
 app.delete("/books/:id", (req, res) => {
   const { id } = req.params;
-
   const bookIndex = books.findIndex((book) => book.id === id);
 
   if (bookIndex >= 0) {
     books.splice(bookIndex, 1);
-    res.json({ message: `Book with ID ${id} successfully deleted.` });
+    res.json(`Book with ID ${id} successfully deleted.`);
   } else {
-    res.status(404).json({ message: `Book with ID ${id} not found.` });
+    res.status(404);
+    res.json(`Book id ${id} not found!`);
   }
 });
 
@@ -41,5 +48,5 @@ app.use((req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`Server Runing on ulr http://localhost:${PORT}`);
 });
